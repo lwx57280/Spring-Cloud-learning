b6e1b6ea8622ba1c9248239dc14ddd0a3c792035
lwx57280 <448341911@qq.com>
2019-02-11 00:19:03 +0800

    【追加注释】

--- chapter2/hystrix-turbine/src/main/java/com/learn/turbine/HystrixTurbineApplication.java
+++ chapter2/hystrix-turbine/src/main/java/com/learn/turbine/HystrixTurbineApplication.java
@@ -3,12 +3,12 @@ package com.learn.turbine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
-
+// 启动类添加@EnableTurbine，激活对Turbine的支持
 @EnableTurbine
 @SpringBootApplication
 public class HystrixTurbineApplication {
 
-	//访问Turbine
+	// 访问Turbine
 	// http://192.168.1.101:8031/turbine.stream?cluster=CONSUMER-MOVIE-RIBBON-WITH-HYSTRIX
 
 	public static void main(String[] args) {
