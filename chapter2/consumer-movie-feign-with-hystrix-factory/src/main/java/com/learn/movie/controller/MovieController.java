d99bcb091360fdd558261d59b9bdbf576025b6c3
lwx57280 <448341911@qq.com>
2019-02-10 17:26:50 +0800

    【Spring Cloud-Feign使用fallbackFactory属性打印fallback异常】

--- chapter2/consumer-movie-feign-with-hystrix-factory/src/main/java/com/learn/movie/controller/MovieController.java
+++ chapter2/consumer-movie-feign-with-hystrix-factory/src/main/java/com/learn/movie/controller/MovieController.java
@@ -4,6 +4,7 @@ package com.learn.movie.controller;
 import com.learn.movie.UserFeignClient;
 import com.learn.movie.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
