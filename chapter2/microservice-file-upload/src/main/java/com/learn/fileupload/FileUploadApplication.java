4ea443bd7904baf62aa4d43e2887719398f63e53
lwx57280 <448341911@qq.com>
2019-02-11 22:32:16 +0800

    【Spring Cloud Zuul简单文件上传微服务采取curl】

---
+++ chapter2/microservice-file-upload/src/main/java/com/learn/fileupload/FileUploadApplication.java
@@ -0,0 +1,16 @@
+package com.learn.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class FileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadApplication.class, args);
+	}
+
+}
+
---
+++ chapter2/microservice-file-upload/src/test/java/com/learn/fileupload/FileUploadApplicationTests.java
@@ -0,0 +1,17 @@
+package com.learn.fileupload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileUploadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
