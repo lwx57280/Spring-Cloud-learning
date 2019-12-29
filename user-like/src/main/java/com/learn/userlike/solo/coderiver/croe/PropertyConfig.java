977a1baec25c4e3b5f1a313c76062a4e948dda6d
lwx457280 <li448341911@qq.com>
2019-12-29 21:47:28 +0800

    【从Spring中容器去取到配置文件】

--- user-like/src/main/java/com/learn/userlike/solo/coderiver/croe/PropertyConfig.java
+++ user-like/src/main/java/com/learn/userlike/solo/coderiver/croe/PropertyConfig.java
@@ -1,45 +1,49 @@
 package com.learn.userlike.solo.coderiver.croe;
 
 import com.learn.userlike.solo.coderiver.util.LocaleUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
-import java.util.Locale;
-
 @Component
-public class PropertyConfig  {
+public class PropertyConfig  implements ApplicationContextAware {
 
-    @Autowired
-    private static ReloadableResourceBundleMessageSource messageSource;
-
+    private static MessageSource messageSource;
 
     /**
-     * 系统语言环境，默认为中文zh
+     *
+     * 服务器启动，Spring容器初始化时，当加载了当前类为bean组件后，
+     * 将会调用下面方法注入ApplicationContext实例
      */
-    public static final String LANGUAGE = "zh";
+    private static ApplicationContext applicationContextFactory;
 
-    /**
-     * 系统国家环境，默认为中国CN
-     */
-    public static final String COUNTRY = "CN";
-    private static Locale getLocale() {
-        Locale locale = new Locale(LANGUAGE, COUNTRY);
-        return locale;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContextFactory = applicationContext;
+    }
+    private static void init() {
+        if (messageSource == null) {
+            synchronized (PropertyConfig.class) {
+                messageSource = (MessageSource) applicationContextFactory.getBean("messageSource");
+            }
+        }
     }
 
     public static String getProperty(String id) {
-        return getMessage(id, null);
+        return getMessage(id, new Object[0]);
     }
 
     public static String getProperty(String code, Object[] param) {
+        init();
         return messageSource.getMessage(code, param, LocaleUtils.getCurrentLocale());
     }
 
     public static String getMessage(String code, Object[] args) {
+        init();
         return messageSource.getMessage(code, args, LocaleUtils.getCurrentLocale());
     }
-
 
     /**
      * 获取配置文件内容
@@ -51,7 +55,8 @@ public class PropertyConfig  {
      * @exception   
      * @date        2019/12/24 20:42
      */
-    public static String getMessage(String code, Object[] args, String defaultMessage) {
+    public static String getProperty(String code, Object[] args, String defaultMessage) {
+        init();
         return messageSource.getMessage(code, args, defaultMessage, LocaleUtils.getCurrentLocale());
     }
 }
