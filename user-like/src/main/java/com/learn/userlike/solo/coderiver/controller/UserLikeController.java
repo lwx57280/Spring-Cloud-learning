55984fb86d979f2cd91cc611eda66dde3535c56f
lwx457280 <li448341911@qq.com>
2020-03-22 21:33:01 +0800

    【使用Redis实现高并发分布式锁】

--- user-like/src/main/java/com/learn/userlike/solo/coderiver/controller/UserLikeController.java
+++ user-like/src/main/java/com/learn/userlike/solo/coderiver/controller/UserLikeController.java
@@ -18,6 +18,9 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.RedissonRedLock;
+import org.redisson.api.RLock;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
@@ -38,6 +41,10 @@ public class UserLikeController {
 
     @Autowired
     private RedisService redisService;
+
+    // redisson 实现分布式锁
+    @Autowired
+    private Redisson redisson;
 
     @Autowired
     private ProductService productService;
@@ -189,5 +196,41 @@ public class UserLikeController {
             },"lock").start();
         }
         return ResultVOUtils.success();
+    }
+
+    /**
+     * 分布式锁Demo
+     * @return
+     */
+    @RequestMapping("readLock")
+    public String readLock() {
+        String lockKey = "product_001";
+        // 这里需要自己实例化不同redi实例的redisson客户端连接，这里只是伪代码用一个redisson客户端简化
+        RLock lock1 = redisson.getLock(lockKey);
+        RLock lock2 = redisson.getLock(lockKey);
+        RLock lock3 = redisson.getLock(lockKey);
+
+        /**
+         * 根据多个RLock对象构建RedissonRedLock(最核心的差别就在这里)
+         * Redis 底层大量使用了lua脚本
+         *
+         */
+        RedissonRedLock redissonLock = new RedissonRedLock(lock1, lock2, lock3);
+        try {
+            /**
+             * waitTimeout尝试获取锁的最大等待时间，超过这个值，则认为获取锁失败
+             * leaseTime 锁的持有时间，超过这个时间锁会自动失效（值应设置为大于业务处理的时间，确保在锁有效期内业务能处理完成）
+             */
+            boolean res = redissonLock.tryLock(10, 30, TimeUnit.SECONDS);
+            if (res) {
+                // 成功获得锁，在这里处理业务
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("lock fail");
+        }finally {
+            // 无论如何，最后都要释放锁
+            redissonLock.unlock();
+        }
+        return null;
     }
 }
